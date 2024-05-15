11cd9a51c251f3dba47a5a01c89edd31e9dac216
akshtshrma24 <akshitsharmaa65@gmail.com>
2024-05-15 00:21:58 -0700

    from alias

--- reloadReadme.py
+++ reloadReadme.py
@@ -1,6 +1,27 @@
+import re
 import os
+
+def atof(text):
+    try:
+        retval = float(text)
+    except ValueError:
+        retval = text
+    return retval
+
+def natural_keys(text):
+    '''
+    alist.sort(key=natural_keys) sorts in human order
+    http://nedbatchelder.com/blog/200712/human_sorting.html
+    (See Toothy's implementation in the comments)
+    float regex comes from https://stackoverflow.com/a/12643073/190597
+    '''
+    return [ atof(c) for c in re.split(r'[+-]?([0-9]+(?:[.][0-9]*)?|[.][0-9]+)', text) ]
+
 dir_list = os.listdir('./notes')
 dir_list2 = os.listdir('./problems')
+
+dir_list.sort(key=natural_keys)
+dir_list2.sort(key=natural_keys)
 
 to_write = ['# Notes for Leetcode\n', 'Welcome to my Leetcode Problems, feel free to clone and just delete my work to do it yourself<br>\n', '`python3 reloadReadme.py` to reload the readMe\n', '\n', '## Notes\n']
 f = open('README.md', 'w')
