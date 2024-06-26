9b8edc7465a7a1a922c85d11e3038f1244a298a4
akshtshrma24 <akshitsharmaa65@gmail.com>
2024-06-26 02:09:57 -0700

    from alias

--- test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <unordered_set>
-#include <utility>
-
-int main() {
-    std::unordered_set<std::pair<int,int>> set;
-    set.insert(std::make_pair(1,1));
-    return 0;
-}
